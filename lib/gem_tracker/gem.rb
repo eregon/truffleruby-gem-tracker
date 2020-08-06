dcdb9ed838c42c769905661fe3a6bb7de50ab071
Benoit Daloze <eregontp@gmail.com>
2020-08-07 00:56:05 +0200

    Ignore cancelled workflow runs

--- lib/gem_tracker/gem.rb
+++ lib/gem_tracker/gem.rb
@@ -137,7 +137,7 @@ module GemTracker
             runs.each do |r|
               jobs = get_run_jobs(r["jobs_url"])
               jobs.each do |j|
-                if j["name"].include?(pattern)
+                if j["name"].include?(pattern) and j["conclusion"] != "cancelled"
                   url = j["html_url"]
                   status = if j["status"] == "in_progress"
                     :in_progress
